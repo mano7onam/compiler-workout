6f5b889186c61d10ce82ac0d04c17e32433a724e
Andrey <amatveev@ccfit.nsu.ru>
2018-03-03 11:53:42 +0700

    HW 3 added files

--- src/SM.ml
+++ src/SM.ml
@@ -1,4 +1,5 @@
 open GT       
+open Language
        
 (* The type for the stack machine instructions *)
 @type insn =
@@ -15,7 +16,7 @@ type prg = insn list
 (* The type for the stack machine configuration: a stack and a configuration from statement
    interpreter
  *)
-type config = int list * Syntax.Stmt.config
+type config = int list * Stmt.config
 
 (* Stack machine interpreter
 
@@ -25,12 +26,21 @@ type config = int list * Syntax.Stmt.config
  *)                         
 let eval _ = failwith "Not yet implemented"
 
+(* Top-level evaluation
+
+     val run : prg -> int list -> int list
+
+   Takes an input stream, a program, and returns an output stream this program calculates
+*)
+let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
+
 (* Stack machine compiler
 
-     val compile : Syntax.Stmt.t -> prg
+     val compile : Language.Stmt.t -> prg
 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-
 let compile _ = failwith "Not yet implemented"
+
+                         
