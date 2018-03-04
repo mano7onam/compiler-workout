df4f8606bbe1166dc530f4f01756a91b06ab8389
Andrey <amatveev@ccfit.nsu.ru>
2018-03-04 15:51:12 +0700

    SM.ml modified

--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,35 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let perform_inst inst ((st, (s, i, o)) : config) = 
+  match inst with
+  | BINOP op -> (
+    match st with 
+    | y :: x :: st_tail -> ((Language.Expr.eval_binop op x y) :: st_tail, (s, i ,o)) 
+    | _ -> failwith "[BINOP]: Too few arguments on stack"		
+  )
+  | CONST n -> (n :: st, (s, i, o))
+  | READ -> (
+    match i with
+    | z :: i_tail -> (z :: st, (s, i_tail, o))
+    | _ -> failwith "[READ]: Too few arguments in input stream"
+  )
+  | WRITE -> (
+    match st with
+    | z :: st_tail -> (st_tail, (s, i, o @ [z]))
+    | _ -> failwith "[WRITE]: Too few arguments on stack"
+  )
+  | LD x -> ((s x) :: st, (s, i, o))
+  | ST x -> (
+    match st with
+    | z :: st_tail -> (st_tail, (Language.Expr.update x z s, i, o))
+    | _ -> failwith "[ST]: Too few arguments on stack"
+  )
+                      
+let rec eval conf prog = 
+  match prog with
+  | [] -> conf	
+  | inst :: tail -> eval (perform_inst inst conf) tail
 
 (* Top-level evaluation
 
@@ -41,4 +69,15 @@ let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compile_expr (expr : Language.Expr.t) = 
+  match expr with
+  | Const n -> [CONST n]
+  | Var x -> [LD x]
+  | Binop (op, x, y) -> compile_expr x @ compile_expr y @ [BINOP op]
+
+let rec compile (stmt : Language.Stmt.t) =
+  match stmt with
+  | Assign (x, e) -> (compile_expr e) @ [ST x]
+  | Read x -> READ :: [ST x]
+  | Write e -> (compile_expr e) @ [WRITE]
+  | Seq (a, b) -> (compile a) @ (compile b)
